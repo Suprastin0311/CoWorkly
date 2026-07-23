074dc0dff2f57057e7b33bf593e8ba0c57881afb
Suprastin0311 <dd.nikolaenko@gmail.com>
2026-07-23 09:22:38 +0300

    Merge branch 'db' into 'master'

--- src/main/java/com/ddnik/Main.java
+++ src/main/java/com/ddnik/Main.java
@@ -3,9 +3,6 @@ package com.ddnik;
 import com.ddnik.controller.AuthController;
 import com.ddnik.controller.MainController;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Main {
 
     public static void main(String[] args) {
