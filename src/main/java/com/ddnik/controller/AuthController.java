b0e4830a662242b5ec6bee74765abd54a180639b
Suprastin0311 <dd.nikolaenko@gmail.com>
2026-07-25 12:13:09 +0300

    Refactor AuthController

--- src/main/java/com/ddnik/controller/AuthController.java
+++ src/main/java/com/ddnik/controller/AuthController.java
@@ -20,7 +20,7 @@ public class AuthController {
 
         do {
             try {
-                System.out.print("Login: ");
+                System.out.print("Email: ");
                 String email = scanner.next().trim();
                 System.out.print("Password: ");
                 String password = scanner.next().trim();
@@ -28,11 +28,11 @@ public class AuthController {
                 if (user != null) {
                     if (user.getRole().equals("admin")) {
                         //TODO получить пользователя
-                        return new AuthorizedUser("suprastin", UserRole.Admin, false);
+                        return new AuthorizedUser("dd.nikolaenko@gmail.com", UserRole.Admin, false);
                     }
                     else if (user.getRole().equals("user")) {
                         //TODO получить пользователя
-                        return new AuthorizedUser("just_user", UserRole.User, false);
+                        return new AuthorizedUser("dd.nikolaenko@yandex.ru", UserRole.User, false);
                     }
                 }
                 else {
@@ -41,7 +41,6 @@ public class AuthController {
                 scanner.close();
 
             } catch (SQLException e) {
-                System.out.println("Ошибка: " + e.getMessage());
                 throw new  Exception(e.getLocalizedMessage(), e);
             }
         } while (true);
