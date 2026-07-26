f04d6ed029a285266f2d5eb08d0dbff72924362b
Suprastin0311 <dd.nikolaenko@gmail.com>
2026-07-26 11:37:36 +0300

    Unused enums deleted

--- src/main/java/com/ddnik/enums/AdminStates.java
+++
@@ -1,7 +0,0 @@
-package com.ddnik.enums;
-
-public enum AdminStates {
-    SEE_BOOKINGS,
-    SEE_WORKSPACES, EDIT_WORKSPACE, CREATE_WORKSPACE,
-    SEE_USERS, MANAGE_USER
-}
--- src/main/java/com/ddnik/enums/UserStates.java
+++
@@ -1,5 +0,0 @@
-package com.ddnik.enums;
-
-public enum UserStates {
-    MAIN_MENU, SEE_BOOKINGS, SEE_WORKSPACES, BOOK_WORKSPACE, CANCEL_BOOKING
-}
