89dd2470a617cf74860aa716d1061917b4723af5
Suprastin0311 <dd.nikolaenko@gmail.com>
2026-08-26 01:13:25 +0300

    Added UserRolesDto

--- src/main/java/com/ddnik/db/dto/UsersDto.java
+++ src/main/java/com/ddnik/db/dto/UsersDto.java
@@ -8,11 +8,20 @@ public record UsersDto (
         String email,
         String passwordHash,
         String fullName,
-        String role,
+        UserRolesDto role,
         boolean isBlocked,
         Date createdAt
 
 ) implements IDto {
+
+    @Override
+    public String toMenuTableRow() {
+        return String.format("%s | %s | %s | %s | %tF %<tR", email, fullName, role.name(), isBlocked ? "Заблокирован" : "Активен", createdAt);
+    }
+
+    public static String getMenuTableHeader() {
+        return "№ | Email | ФИО | Роль | Статус | Дата регистрации";
+    }
 
     public UsersDto {
         Objects.requireNonNull(email);
@@ -20,14 +29,5 @@ public record UsersDto (
         Objects.requireNonNull(fullName);
         Objects.requireNonNull(role);
         Objects.requireNonNull(createdAt);
-    }
-
-    @Override
-    public String toMenuTableRow() {
-        return String.format("%s | %s | %s | %s | %tF %<tR", email, fullName, role, isBlocked ? "Заблокирован" : "Активен", createdAt);
-    }
-
-    public static String getMenuTableHeader() {
-        return " № | Email | ФИО | Роль | Статус | Дата регистрации";
     }
 }
