b89068b9b04409cdabfe6d6108f3e6ed6cf976f0
Suprastin0311 <dd.nikolaenko@gmail.com>
2026-08-25 19:16:47 +0300

    Logger added to Main class

--- src/main/java/com/ddnik/Main.java
+++ src/main/java/com/ddnik/Main.java
@@ -2,10 +2,20 @@ package com.ddnik;
 
 import com.ddnik.controller.ConsoleReader;
 import com.ddnik.controller.MainController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Main {
 
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
     public static void main(String[] args) {
+        Thread.setDefaultUncaughtExceptionHandler((thread, throwable) -> {
+            logger.error("Необработанное исключение в потоке '{}'", thread.getName(), throwable);
+
+            System.err.println("Произошла непредвиденная ошибка. Работа программы будет прекращена.");
+        });
+
         MainController main = new MainController();
         main.start();
         ConsoleReader.cls();
